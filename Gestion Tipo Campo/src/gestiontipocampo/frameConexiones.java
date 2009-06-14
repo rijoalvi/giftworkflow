ed481d3276e08384ad9b805c3b75edc84206de9d
luiscarlosch@gmail.com <luiscarlosch@gmail.com@8be3c716-5316-11de-9335-abd5230c83eb>
2009-06-14 06:36:34 +0000

    ya no se probarán las conexiones al principio, porque duran mucho, deporsí ya sabemos cual sirve según el lugar donde estemos... entonces escojamos la que corresponda

--- Gestion Tipo Campo/src/gestiontipocampo/frameConexiones.java
+++ Gestion Tipo Campo/src/gestiontipocampo/frameConexiones.java
@@ -61,7 +61,6 @@ public class frameConexiones extends javax.swing.JFrame {
         lblConexion2.setName("lblConexion2"); // NOI18N
 
         jRadioButton1.setText(resourceMap.getString("opt1.text")); // NOI18N
-        jRadioButton1.setEnabled(false);
         jRadioButton1.setName("opt1"); // NOI18N
         jRadioButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -75,7 +74,6 @@ public class frameConexiones extends javax.swing.JFrame {
         });
 
         jRadioButton2.setText(resourceMap.getString("opt2.text")); // NOI18N
-        jRadioButton2.setEnabled(false);
         jRadioButton2.setName("opt2"); // NOI18N
         jRadioButton2.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -131,14 +129,14 @@ public class frameConexiones extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblConexion2)
                     .addComponent(lblConexion1))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2))
+                    .addComponent(jRadioButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(43, 43, 43)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnEstablecerConexion)
@@ -161,8 +159,8 @@ public class frameConexiones extends javax.swing.JFrame {
         ControladorBD probadorConexiones = new ControladorBD();
 
         //ATENCION: YA NO HAY QUE COMENTAR NADA AQUÍ*********************************
-        System.out.println("Probando 2");
-        if (1 == probadorConexiones.probarConexion(2)) {
+        System.out.println("Conexiones no probadas, decida ud cual podría estar disponible");
+        /*if (1 == probadorConexiones.probarConexion(2)) {
             jRadioButton2.setText("Disponible");
             jRadioButton2.setEnabled(true);
 
@@ -180,7 +178,7 @@ public class frameConexiones extends javax.swing.JFrame {
                 jRadioButton1.setText("Disponible");
             //  jRadioButton1.setBackground(Color.green);
             }
-        }
+        }*/
 
         jLabel1.setText("Por favor elija la Base de Datos a la que desea conectarse.");
 
