c71b7c50a158f3137793db7393afbab88b0d263c
tarassenkor <tarassenkor@8be3c716-5316-11de-9335-abd5230c83eb>
2009-06-29 00:18:07 +0000

    Mas avances del flujo, ya cree las tablas de FLUJO en MySQL

--- Gestion Tipo Campo/src/gestiontipocampo/ConsultaFlujo.java
+++ Gestion Tipo Campo/src/gestiontipocampo/ConsultaFlujo.java
@@ -4,6 +4,8 @@
  */
 package gestiontipocampo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Vector;
 
 /**
@@ -22,14 +24,41 @@ public abstract class ConsultaFlujo extends ControladorBD {
      * @param correlativoRaiz - correlativo de la actividad en la raiz del flujo
      * @return correlativo del flujo recien creado.
      */
-    public abstract int nuevoFlujo(String nombre, String descripcion, int correlativoRaiz);
+    public int nuevoFlujo(String nombre, String descripcion, int correlativoRaiz) {
+        int correlativo = -1;
+        String[] incremental = {"correlativo"};
+        ResultSet resultado = null;
+        try {
+            resultado = this.doUpdate("insert into FLUJO (nombre, descripcion, actividadRaiz) values ('" + nombre + "', '" + descripcion + "', " + correlativoRaiz + ";", incremental);
+            while (resultado.next()) {
+                correlativo = resultado.getInt(1);
+            }
+        } catch (SQLException e) {
+            System.out.println("*SQL Exception: *" + e.toString());
+        }
+        return correlativo;
+    }
 
     /**
      * Metodo que recupera datos de un flujo ya creado y devuelve los datos en un vector
      * @param correlativo
      * @return //Los datos tienen que ir en el siguiente orden: nombre, descripcion, raiz
      */
-    public abstract Vector<String> getFlujo(int correlativo);
+    public Vector<String> getFlujo(int correlativo) {
+        Vector<String> retorno = new Vector<String>();
+        ResultSet resultado = null;
+        try {
+            resultado = this.getResultSet("select nombre, descripcion, actividadRaiz from FLUJO where correlativo = " + correlativo + ";");
+            while (resultado.next()) {
+                retorno.add(resultado.getString("nombre"));
+                retorno.add(resultado.getString("descripcion"));
+                retorno.add(resultado.getString("actividadRaiz"));
+            }
+        } catch (SQLException e) {
+            System.out.println("*SQL Exception: *" + e.toString());
+        }
+        return retorno;
+    }
 
     /**
      * Matodo para actualizar los datos de un flujo ya creado en la BD
@@ -38,5 +67,7 @@ public abstract class ConsultaFlujo extends ControladorBD {
      * @param descripcion
      * @param raiz
      */
-    public abstract void actualizar(int correlativo, String nombre, String descripcion, int raiz);
+    public void actualizar(int correlativo, String nombre, String descripcion, int raiz) {
+        this.doUpdate("UPDATE FLUJO set nombre = '" + nombre + "', descripcion= '" + descripcion + "', actividadRaiz = '" + raiz + "' WHERE correlativo = " + correlativo + ";");
+    }
 }
